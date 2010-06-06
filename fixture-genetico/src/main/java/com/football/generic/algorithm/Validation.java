ba3f5a1cf1633ff289fa1ef07ad31e834621c297
rinaudo.ludmila <rinaudo.ludmila@eb731fb5-9e41-5dd8-e214-570ae9e8f031>
2010-06-06 18:15:36 +0000

    initial commit

---
+++ fixture-genetico/src/main/java/com/football/generic/algorithm/Validation.java
@@ -0,0 +1,18 @@
+package main.java.com.football.generic.algorithm;
+
+import main.java.com.football.fixture.SoccerGame;
+
+/*
+ * Esta clase se encarga de cumplir el rol de la funcion de aptitud
+ */
+
+public class Validation {
+
+	public Validation(){
+		
+	}
+	
+	public boolean isValid(){
+		return true;
+	}
+}
