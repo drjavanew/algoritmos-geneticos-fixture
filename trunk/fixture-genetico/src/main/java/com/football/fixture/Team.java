daf4efd817f47df00693f84532f82713a2fcd6f8
duilio.hernandez <duilio.hernandez@eb731fb5-9e41-5dd8-e214-570ae9e8f031>
2010-06-11 01:49:27 +0000

    ver si un fixture tiene partidos repetidos

--- trunk/fixture-genetico/src/main/java/com/football/fixture/Team.java
+++ trunk/fixture-genetico/src/main/java/com/football/fixture/Team.java
@@ -3,14 +3,15 @@ package main.java.com.football.fixture;
 public class Team {
 
 	private String name;
-	private boolean isBigTeam; // puede ser un numero de 1 a 10, siendo 10 para los equipos mas populares
-	
-	public Team(String name, boolean isBigTeam){
+	private boolean isBigTeam; // puede ser un numero de 1 a 10, siendo 10 para
+								// los equipos mas populares
+
+	public Team(String name, boolean isBigTeam) {
 		this.name = name;
 		this.isBigTeam = isBigTeam;
 	}
-	
-	public Team(Team team){
+
+	public Team(Team team) {
 		this.name = team.name;
 		this.isBigTeam = team.isBigTeam;
 	}
@@ -22,6 +23,30 @@ public class Team {
 	public boolean IsBigTeam() {
 		return isBigTeam;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Team other = (Team) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
